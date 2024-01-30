d6d18df4f2bd8414feffee7a28950023821c9f65
Jeffrey <jeffrey.popek@gmail.com>
2024-01-30 00:59:11 -0500

    Fixed Bug, Finished Assignment
    Rearranged code and fixed the bug

--- assignments/assignment0/main.cpp
+++ assignments/assignment0/main.cpp
@@ -2,13 +2,6 @@
 #include <math.h>
 
 #include <ew/external/glad.h>
-
-#include <GLFW/glfw3.h>
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
-
-// Assignment 0
 #include <ew/shader.h>
 #include <ew/model.h>
 #include <ew/camera.h>
@@ -16,10 +9,23 @@
 #include <ew/cameraController.h>
 #include <ew/texture.h>
 
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
-ew::CameraController cameraController;
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+GLFWwindow* initWindow(const char* title, int width, int height);
+void drawUI();
 
 ew::Camera camera;
+ew::CameraController cameraController;
+
+//Global state
+int screenWidth = 1080;
+int screenHeight = 720;
+float prevFrameTime;
+float deltaTime;
 
 struct Material {
 	float Ka = 1.0;
@@ -28,28 +34,14 @@ struct Material {
 	float Shininess = 128;
 }material;
 
-
-
-void framebufferSizeCallback(GLFWwindow* window, int width, int height);
-GLFWwindow* initWindow(const char* title, int width, int height);
-void drawUI();
-
-//Global state
-int screenWidth = 1080;
-int screenHeight = 720;
-float prevFrameTime;
-float deltaTime;
-
-
-
-
-
 int main() {
 	GLFWwindow* window = initWindow("Assignment 0", screenWidth, screenHeight);
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK); //Back face culling
+	glEnable(GL_DEPTH_TEST); //Depth testing
 
-	// Assigment 0
 	ew::Shader shader = ew::Shader("assets/lit.vert", "assets/lit.frag");
 	ew::Model monkeyModel = ew::Model("assets/suzanne.obj");
 
@@ -58,11 +50,10 @@ int main() {
 	camera.aspectRatio = (float)screenWidth / screenHeight;
 	camera.fov = 60.0f; //Vertical field of view, in degrees
 
-	ew::Transform monkeyTransform;
-
 	//Handles to OpenGL object are unsigned integers
 	GLuint brickTexture = ew::loadTexture("assets/brick_color.jpg");
 
+	ew::Transform monkeyTransform;
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -71,47 +62,36 @@ int main() {
 		deltaTime = time - prevFrameTime;
 		prevFrameTime = time;
 
-		//RENDER
-		glClearColor(0.6f,0.8f,0.92f,1.0f);
-
-
-		// Assignment 0
-		//Clears backbuffer color & depth values
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		shader.use();
-		shader.setMat4("_Model", glm::mat4(1.0f));
-		shader.setMat4("_ViewProjection", camera.projectionMatrix() * camera.viewMatrix());
-		monkeyModel.draw(); //Draws monkey model using current shader
-
-
-		// BUG: NOT WORKING, IDK WHY
-		//Rotate model around Y axis
-		monkeyTransform.rotation = glm::rotate(monkeyTransform.rotation, deltaTime, glm::vec3(0.0, 1.0, 0.0));
-
-		//transform.modelMatrix() combines translation, rotation, and scale into a 4x4 model matrix
-		shader.setMat4("_Model", monkeyTransform.modelMatrix());
-
 		cameraController.move(window, &camera, deltaTime);
 
+		//RENDER
+		glClearColor(0.6f, 0.8f, 0.92f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		//Bind brick texture to texture unit 0 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, brickTexture);
 		//Make "_MainTex" sampler2D sample from the 2D texture bound to unit 0
 		shader.use();
-		shader.setInt("_MainTex", 0);
-
 		shader.setVec3("_EyePos", camera.position);
+		shader.setInt("_MainTex", 0);
+		shader.setMat4("_Model", glm::mat4(1.0f));
+		shader.setMat4("_ViewProjection", camera.projectionMatrix() * camera.viewMatrix());
 
 		shader.setFloat("_Material.Ka", material.Ka);
 		shader.setFloat("_Material.Kd", material.Kd);
 		shader.setFloat("_Material.Ks", material.Ks);
 		shader.setFloat("_Material.Shininess", material.Shininess);
 
+		//Rotate model around Y axis
+		monkeyTransform.rotation = glm::rotate(monkeyTransform.rotation, deltaTime, glm::vec3(0.0, 1.0, 0.0));
+
+		//transform.modelMatrix() combines translation, rotation, and scale into a 4x4 model matrix
+		shader.setMat4("_Model", monkeyTransform.modelMatrix());
+
+		monkeyModel.draw(); // Draws monkey model using current shader
 
 		drawUI();
-
 
 		glfwSwapBuffers(window);
 	}
@@ -124,14 +104,12 @@ void resetCamera(ew::Camera* camera, ew::CameraController* controller) {
 	controller->yaw = controller->pitch = 0;
 }
 
-
 void drawUI() {
 	ImGui_ImplGlfw_NewFrame();
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui::NewFrame();
 
 	ImGui::Begin("Settings");
-
 	if (ImGui::Button("Reset Camera")) {
 		resetCamera(&camera, &cameraController);
 	}
@@ -143,13 +121,10 @@ void drawUI() {
 		ImGui::SliderFloat("Shininess", &material.Shininess, 2.0f, 1024.0f);
 	}
 
-
 	ImGui::End();
 
 	ImGui::Render();
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-
 }
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
@@ -180,25 +155,16 @@ GLFWwindow* initWindow(const char* title, int width, int height) {
 	}
 	glfwMakeContextCurrent(window);
 
-
-
 	if (!gladLoadGL(glfwGetProcAddress)) {
 		printf("GLAD Failed to load GL headers");
 		return nullptr;
 	}
-
-	glEnable(GL_CULL_FACE);
-	glCullFace(GL_BACK); //Back face culling
-	glEnable(GL_DEPTH_TEST); //Depth testing
 
 	//Initialize ImGUI
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init();
-
-
-
 
 	return window;
 }
